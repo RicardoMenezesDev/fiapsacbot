fd4bdc16a20a63495d882213bd8ea4c6e9447835
Home Studio <homestudio@Mac-mini-de-Home.local>
2020-07-20 23:07:48 -0300

    Versionamento inicial do projeto FiapSacBot

---
+++ src/test/java/br/com/fiap/fiapsacbot/FiapsacbotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapsacbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapsacbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
