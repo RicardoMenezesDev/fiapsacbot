8d2f4e46b014ba53c0f8e0394046c476a9e6856e
guirsantos <guirsantos@gmail.com>
2020-07-24 14:16:24 -0300

    Alteracao das mensagens iniciais

--- src/main/java/br/edu/fiap/fiapsacbot/configuracao/MensagemEnum.java
+++ src/main/java/br/edu/fiap/fiapsacbot/configuracao/MensagemEnum.java
@@ -1,20 +1,29 @@
 package br.edu.fiap.fiapsacbot.configuracao;
 
 public enum MensagemEnum {
-    BEM_VINDO("\nSeja bem vindo(a) ao Serviço de Atendimento da FIAP."),
+    BEM_VINDO("\nSeja bem vindo(a) ao Servi?o de Atendimento da FIAP."),
     BEM_VINDO_AJUDA("\nComo podemos ajudar?"),
-    OPCOES("\nPara facilitar, temos as seguintes opções para ajudar a direcionar sua busca:\n" +
+    OPCOES("\nPara facilitar seu atendimento consulte as op??es abaixo:\n" +
             "\n - AULAS" +
             "\n - APOSTILAS" +
-            "\n - CALENDÁRIO DE AULAS" +
+            "\n - CALEND?RIO DE AULAS" +
             "\n - ENTREGA DE TRABALHOS" +
             "\n - BOLETO" +
             "\n - ALTERAR SENHA" +
             "\n - PROFESSORES" +
             "\n - DISCIPLINAS" +
             "\n - NOTAS"),
-    NAO_ENTENDI(" não entendi muito bem, mas oh! atualmente o que eu consigo te informar é sobre os tópicos: aula, calendário de aulas. \nSe for isso, digita aqui o tópico, tudo bem?"),
-    MATRICULA("\nInforme sua matrícula para iniciar o atendimento.");
+    NAO_ENTENDI(" Desculpe, não foi possível realizar sua solicitação. Informe uma das opções abaixo para proseguir com o atendimento." +
+            "\n - AULAS" +
+            "\n - APOSTILAS" +
+            "\n - CALEND?RIO DE AULAS" +
+            "\n - ENTREGA DE TRABALHOS" +
+            "\n - BOLETO" +
+            "\n - ALTERAR SENHA" +
+            "\n - PROFESSORES" +
+            "\n - DISCIPLINAS" +
+            "\n - NOTAS"),
+    MATRICULA("\nInforme sua matr?cula para iniciar o atendimento.");
 
     private final String descricao;
 
