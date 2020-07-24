ab4aa17e6024bd2badb4a7a8c2996f3166e9723e
Danilo3xM <danilo.3xm@gmail.com>
2020-07-23 23:50:04 -0300

    FluxoMensagem - Ajustado tratamento de mensagens para usar o switch case.7 MensagemEnum - Adicionado "OPCOES" Geral: - Correções de encoding para ISO 8859-1 para aceitar caracteres Brs. - Indentação.

--- src/main/java/br/edu/fiap/fiapsacbot/Main.java
+++ src/main/java/br/edu/fiap/fiapsacbot/Main.java
@@ -2,19 +2,21 @@ package br.edu.fiap.fiapsacbot;
 
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.updates.GetUpdates;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import br.edu.fiap.fiapsacbot.servico.FiapSacBot;
+import org.telegram.telegrambots.meta.generics.BotSession;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
 
-    public static void main (String []args)
-    {
+    public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
         try {
             telegramBotsApi.registerBot(new FiapSacBot());
-
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
--- src/main/java/br/edu/fiap/fiapsacbot/servico/DicionarioSinonimos.java
+++ src/main/java/br/edu/fiap/fiapsacbot/servico/DicionarioSinonimos.java
@@ -2,13 +2,14 @@ package br.edu.fiap.fiapsacbot.servico;
 
 public class DicionarioSinonimos {
 
-	public DicionarioSinonimos() {}
-	
-	public String sinonimoPalavra(String palavraOrigem) {
-		if (palavraOrigem.equals("classe") || palavraOrigem.equals("colégio") ||palavraOrigem.equals("colegio") || palavraOrigem.equals("escola")
-				|| palavraOrigem.equals("sala") || palavraOrigem.equals("turma") || palavraOrigem.equals("curso")) {
-			return "aula";
-		}
-		return "";
-	}
+    public DicionarioSinonimos() {
+    }
+
+    public String sinonimoPalavra(String palavraOrigem) {
+        if (palavraOrigem.equals("classe") || palavraOrigem.equals("colégio") || palavraOrigem.equals("colegio") || palavraOrigem.equals("escola")
+                || palavraOrigem.equals("sala") || palavraOrigem.equals("turma") || palavraOrigem.equals("curso")) {
+            return "aula";
+        }
+        return palavraOrigem;
+    }
 }
